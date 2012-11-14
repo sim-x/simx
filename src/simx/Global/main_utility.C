f88ebe845775044e1b3cc4e3c992508aa9d01472
Sunil Thulasidasan <sunil@lanl.gov>
2012-11-14 13:08:57 -0700

    Added copyright notice to source files.

--- src/simx/Global/main_utility.C
+++ src/simx/Global/main_utility.C
@@ -1,3 +1,21 @@
+// Copyright (c) 2012. Los Alamos National Security, LLC. 
+
+// This material was produced under U.S. Government contract DE-AC52-06NA25396
+// for Los Alamos National Laboratory (LANL), which is operated by Los Alamos 
+// National Security, LLC for the U.S. Department of Energy. The U.S. Government 
+// has rights to use, reproduce, and distribute this software.  
+
+// NEITHER THE GOVERNMENT NOR LOS ALAMOS NATIONAL SECURITY, LLC MAKES ANY WARRANTY, 
+// EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  
+// If software is modified to produce derivative works, such modified software should
+// be clearly marked, so as not to confuse it with the version available from LANL.
+
+// Additionally, this library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License v 2.1 as published by the 
+// Free Software Foundation. Accordingly, this library is distributed in the hope that 
+// it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See LICENSE.txt for more details.
+
 //--------------------------------------------------------------------------
 // $Id: main_utility.C,v 1.1.1.1 2011/08/18 22:19:46 nsanthi Exp $
 //--------------------------------------------------------------------------
@@ -13,7 +31,7 @@
 //  - very similar to main_dassf.C. All utility type programs that do not need
 // to use PrimeSSF or MPI ( but may need configuration files) should use this.
 // - Sunil ( Oct 23, 2008 )
-// @@COPYRIGHT@@
+// @@
 //
 //--------------------------------------------------------------------------
 
