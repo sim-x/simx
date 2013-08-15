a524f6fded556afe5efa26953cb88152c458bfe2
Sunil Thulasidasan <sunil@lanl.gov>
2013-08-15 14:38:54 -0600

    More changes for a much more robust build system. setup script now respects all user expectations when building and installing a Python package.
    custom install and build commands were added to setup.py for this
    purpose.
    
    One of the quirks of the Python setuptools/disutils is that it is not
    very great at deciding when to recompile extensions. For example, if
    rebuilding by merely changing a build option, setuptools will likely not
    rebuild the extension. To get around this, first do
    
    ./setup.py clean --all
    
    and then do
    
    ./setup.py build <with-different-options>

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ python setup.py install --user
 
 """
 
-#Parts of the setup script are based on the setup.py of PyOpenCV
-
-from setuptools import setup, find_packages, Extension
+from setuptools import setup#, find_packages, Extension
+import distutils.command.build as _build
+import setuptools.command.install as _install
 
 import sys
 import os
@@ -22,38 +22,87 @@ import os.path as op
 import distutils.spawn as ds
 import distutils.dir_util as dd
 
-try:
-    import config as C
-except ImportError: # no config.py file found
+#import distutils
+#import distutils.dist
+
+#################
+# CMake function
+#################
+def run_cmake(cmake_args="-DSIMX_USE_PRIME=1"):
+    """
+    Runs CMake to determine configuration for this build
+    """
     if ds.find_executable('cmake') is None:
         print "CMake  is required to build SimX"
         print "Please install cmake version >= 2.6 and re-run setup"
         sys.exit(-1)
-
+        
     print "Configuring SimX build with CMake.... "
-    cwd = os.getcwd()
     new_dir = op.join(op.split(__file__)[0],'build')
     dd.mkpath(new_dir)
     os.chdir(new_dir)
-
     try:
-        ds.spawn(['cmake','..'])
+        ds.spawn(['cmake','../',cmake_args])
     except ds.DistutilsExecError:
         print "Error while running cmake"
         print "Try editing the settings in CMakeLists.txt file and re-running setup"
         os.chdir(cwd)
         sys.exit(-1)
 
-# try:
-#     ds.spawn(['make'])
-# except ds.DistutilsExecError:
-#     print "Error while building SimX"
-#     os.chdir(cwd)
-#     sys.exit(-1)
+        
+###########################
+# Custom 'install' command
+###########################
+class install(_install.install):
+    def run(self):
+        try:
+            import config as C
+        except ImportError:
+            cwd = os.getcwd()
+            run_cmake()
+            os.chdir(cwd)
+            import config as C
+        self.distribution.ext_modules = C.extension_list
+        # calling _install.install.run(self) does not fetch  required packages
+        # and instead performs an old-style install. see command/install.py in
+        # setuptools. So, calling do_egg_install() manually here.
+        self.do_egg_install()
 
-    os.chdir(cwd)
-    import config as C
 
+#########################
+# Custom 'build' command
+#########################
+class  build(_build.build):
+
+    user_options=_build.build.user_options + \
+        [('without-ssf',None,"Uses SimEngine instead of SSF for message passing and synchronization. \
+ This will only work with multithreaded MPI")]
+
+    def initialize_options(self):
+        _build.build.initialize_options(self)
+        self.without_ssf = 0
+        
+    def run(self):
+        cwd = os.getcwd()
+        if self.without_ssf:
+            run_cmake("-DSIMX_USE_PRIME=0")
+        else:
+            run_cmake()
+        #try:
+        #    import config as C
+        #    with open('build/src/minissf/ssf_config.h'): pass
+        #except (ImportError, IOError): 
+        # either config.py file or ssf_config.h not found. Run CMake
+        os.chdir(cwd)
+        import config as C
+        # Now populate the extension module  attribute.
+        self.distribution.ext_modules = C.extension_list
+        _build.build.run(self)
+
+
+###################
+# setup
+###################
 setup(
     name = "simx",
     version = '1.0',
@@ -86,7 +135,9 @@ setup(
          'Topic :: Software Development :: Libraries :: Python Modules',
          'Topic :: Software Development :: Libraries :: Application Frameworks',
          'Topic :: System :: Distributed Computing'],
-    ext_modules = C.extension_list,
+    # ext_modules is not present here. This will be generated through CMake via the
+    # build or install commands
+    cmdclass={'install':install,'build': build},
     zip_safe=False,
     packages = ['simx'],
     )
