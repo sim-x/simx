63a7eb966f8f8b27a1cdcfc13303889c74c736f4
thulas <sunil@lanl.gov>
2014-03-31 18:33:12 -0600

    Updated copyright year

--- simx/process.py
+++ simx/process.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2012. Los Alamos National Security, LLC. 
+# Copyright (c) 2013, 2014 Los Alamos National Security, LLC. 
 
 # This material was produced under U.S. Government contract DE-AC52-06NA25396
 # for Los Alamos National Laboratory (LANL), which is operated by Los Alamos 
